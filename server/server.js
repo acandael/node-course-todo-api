359f3a5e21f42bb012eaf6008928f7afecceeeab
acandael <info@anthonycandaele.com>
2018-05-13 15:53:20 +0200

    Add DLETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -47,7 +47,31 @@ app.get('/todos/:id', (req, res) => {
     }
   }).catch((e) => {
     res.status(400).send();
-  })
+  });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  };
+
+  // remove todo by id
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.send(todo);
+  }).catch((e) => {
+    res.status(400).send();
+  });
+    // success
+      // if no doc send 404
+      // if doc send dock back with 200
+    // error
+      // 400 with empty body
+
 
 })
 
